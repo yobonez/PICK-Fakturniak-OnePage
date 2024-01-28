8ce253e575a3d3b64d236e4d7d9d41789c598df3
yobonez <59935203+yobonez@users.noreply.github.com>
2024-01-28 18:57:54 +0100

    Proba stworzenia wersji mobilnej - jeszcze nie wiem czy dziala jak trzeba

--- js/util.js
+++ js/util.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", (e) =>
     opinionsElem = document.querySelector(".opinions");
     carouselElem = document.querySelector(".carousel");
 
+    
     carouselElem.addEventListener("wheel", (e) =>{
         e.preventDefault();
         carouselElem.scrollLeft+=e.deltaY;
